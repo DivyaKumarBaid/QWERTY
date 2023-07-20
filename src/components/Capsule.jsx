af32276bb282659231d9547df8693e7d51c27b4f
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-07-20 19:26:04 +0530

    filter screen added

--- src/components/Capsule.jsx
+++ src/components/Capsule.jsx
@@ -6,7 +6,7 @@ export const Capsule = () => {
     const typingContext = useTyping();
 
     return (
-        <div className={`border-[rgba(255,255,255,0.2)] absolute top-[25%] transition-all duration-[0.5s] border py-2 px-4 rounded-3xl bg-[rgba(36,36,36)] shadow-md ${typingContext.isTyping ? 'opacity-0' : 'opacity-100'} font-['Josefin_Sans'] font-light`}>
+        <div className={`border-[rgba(255,255,255,0.2)] absolute top-[20%] transition-all duration-[0.5s] border py-2 px-4 rounded-3xl bg-[rgba(36,36,36)] shadow-md ${typingContext.isTyping ? 'opacity-0' : 'opacity-100'} font-['Josefin_Sans'] font-light`}>
             Hello World
         </div>
     )
--- src/components/Solo.jsx
+++ src/components/Solo.jsx
@@ -8,19 +8,38 @@ const Solo = () => {
     const typingContext = useTyping();
 
     const [input, setInput] = React.useState("");
-    const [index, setIndex] = React.useState(0);
+    const [index, setIndex] = React.useState(-1);
     const [test, setTest] = React.useState(sentence.split(''));
     const [testCorrect, setTestCorrect] = React.useState(sentence.split('').map(i => true));
-    console.log(test);
+    // console.log(test);
 
     const handleInput = (e) => {
         typingContext.setIsTyping(true);
         setInput(e.target.value);
-        const lastChar = (e.target.value[e.target.value.length - 1] === test[e.target.value.length - 1]); //true or false;
-        // console.log(e.target.value[e.target.value.length - 1], test[e.target.value.length - 1], lastChar)
+        let len = e.target.value.length - 1;
+        const lastChar = (e.target.value[len] === test[len]); //true or false;
         const arr = testCorrect;
-        arr[e.target.value.length - 1] = lastChar;
-        setIndex(e.target.value.length - 1);
+        arr[len] = lastChar;
+        setIndex(len);
+        console.log(len, len % 120)
+        // len % 130 == 129 && document.getElementById('scrollDiv').scrollBy(0, 60);
+        if (len == 125) {
+            document.getElementById('scrollDiv').scrollBy({
+                top: 36,
+                left: 0,
+                behavior: "smooth",
+            });
+        }
+        else {
+            if (len > 125) {
+                len -= 120;
+                len % 60 == 0 && document.getElementById('scrollDiv').scrollBy({
+                    top: 36,
+                    left: 0,
+                    behavior: "smooth",
+                });
+            }
+        }
         setTestCorrect(arr);
     }
 
@@ -36,23 +55,26 @@ const Solo = () => {
                     ref={typingContext.inputRef}
                 />
             </div>
-            <div className="typer w-[80%] max-h-[20vh] overflow-auto">
+            <div className='w-[100%] flex justify-center items-center filterScreen'
+                onClick={() => {
+                    typingContext.inputRef.current.focus()
+                    typingContext.setIsTyping(true);
+                    document.body.style.cursor = 'none'
+                }}
+            >
                 <div
-                    className="text-3xl font-['Source_Code_Pro'] text-center tracking-[-8px]"
-                    onClick={() => {
-                        typingContext.inputRef.current.focus()
-                        typingContext.setIsTyping(true);
-                        document.body.style.cursor = 'none'
-                    }}
+                    className="typer scroll-smooth w-[80%] max-h-[38vh] overflow-auto" id="scrollDiv"
                 >
-                    {test.map((item, idx) => {
-                        return (
-                            <span key={item + idx}>
-                                <span className={`${idx > index ? 'opacity-[0.25]' : testCorrect[idx] ? 'opacity-100' : '!text-[red] opacity-100'}`}>{item}</span>
-                                {<span className={`text-white transition-all text-[var(--theme-font-color)] duration-[0.1s] ${(typingContext.isTyping && idx == index) ? 'opacity-100 animate-pulse' : 'opacity-0'}`}>|</span>}
-                            </span>
-                        )
-                    })}
+                    <div className="text-3xl font-['Source_Code_Pro'] my-20 text-center tracking-[-8px]">
+                        {test.map((item, idx) => {
+                            return (
+                                <span key={item + idx}>
+                                    <span className={`${idx > index ? 'opacity-[0.25]' : testCorrect[idx] ? 'opacity-100' : '!text-[red] opacity-100'} `}>{item}</span>
+                                    {<span className={`text-white transition-all text-[var(--theme-font-color)] duration-[0.1s] ${(typingContext.isTyping && idx == index) ? 'opacity-100 animate-pulse' : 'opacity-0'}`}>|</span>}
+                                </span>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </>
