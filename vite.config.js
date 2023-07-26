6c76be317c69d978c943bdba1dd0c5be0ec6cc88
DivyaKumarBaid <divyakumarbaid.dkb.2001@gmail.com>
2023-07-26 16:20:44 +0530

    deno config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite@^3.1.3";
-import react from "@vitejs/plugin-react@^2.1";
-
-import "react@^18.2";
-import "react-dom@^18.2/client";
-import "react-router-dom@^6.4"; // Add this line
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import 'react-router-dom';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-});
+})
